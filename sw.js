881aba97f083c608ab65d8ef3ea81734d022858d
oocnewday <oocnewday@gmail.com>
2026-09-22 10:16:23 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    بدل الجديدة.
    ============================================================ */
 
-const CACHE_NAME = "newday-cache-v5";
+const CACHE_NAME = "newday-cache-v8";
 
 const APP_SHELL = [
   "./index.html",
